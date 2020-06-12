f9aeb6ab9ca908bdb7d376193b84e15a7af357a8
jorge.fan <jorge.fan@geekbang.com>
2020-06-12 11:39:55 +0800

    test2

---
+++ src/views/test/test.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="page-wrap">
+
+  </div>
+</template>
+ 
+<script>
+export default {
+  name: 'page-wrap',
+  components: {
+  },
+  props: {
+  },
+  data () {
+    return {
+
+    }
+  },
+  computed: {
+  },
+  created () {
+  },
+  methods: {
+  }
+}
+</script>
